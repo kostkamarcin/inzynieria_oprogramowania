eefc28e688475aa23f0038a386caed7bf54e50d8
Marcin Kostka <mkostka5@gmail.com>
2023-04-16 17:37:52 +0200

    romb

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -9,4 +9,15 @@ obwod = a + b + c
 pole = int((h * a) / 2)
 
 print("Obwod trojkata wynosi " 
-    + str(obwod) + ", zas pole wynisi " + str(pole) + ".")
+    + str(obwod) + ", zas pole wynosi " + str(pole) + ".")
+
+# romb
+
+a_romb = 9
+h_romb = 12
+
+obwod_romb = 4 * a_romb
+pole_romb = a_romb * h_romb
+
+print("Obwod rombu wynosi " 
+    + str(obwod_romb) + ", zas pole wynosi " + str(pole_romb) + ".")
