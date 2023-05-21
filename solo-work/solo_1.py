cab13e1abd83e764790aedcc495e33b5f685266d
Marcin Kostka <mkostka5@gmail.com>
2023-05-21 09:49:21 +0200

    zadanie_2

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -10,3 +10,10 @@ print("{} {}".format(hello, student))
 student = input("Wpisz swoje imie")
 
 print("{} {}".format(hello, student))
+
+# zadanie 1.3
+
+studenci = ["Ania", "Kuba", "Piotr", "Jan"]
+liczba_studentow = len(studenci)
+
+print("{} {}".format("Liczba studentow wynosi: ", liczba_studentow))
