18e3d749c6bb46b3d40c49ff041c052040349ad0
Marcin Kostka <mkostka5@gmail.com>
2023-05-21 09:57:13 +0200

    zadanie_4

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -17,3 +17,10 @@ studenci = ["Ania", "Kuba", "Piotr", "Marcin"]
 liczba_studentow = len(studenci)
 
 print("{} {}".format("Liczba studentow wynosi: ", liczba_studentow))
+
+# zadanie 1.4
+
+studenci = ["Ania", "Kasia", "Piotr", "Tomek"]
+
+for s in studenci:
+    print(hello, s)
